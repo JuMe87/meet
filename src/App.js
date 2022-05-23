a37c8f3646f93fbbb2090ac3da57126197de9b65
JuMe87 <juliane.meyen87@googlemail.com>
2022-05-23 10:55:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ class App extends Component {
         this.mounted = false
     }
 
+    updateNumberOfEvents = (numberOfEvents) => {
+        this.setState(
+            {
+                numberOfEvents,
+            },
+            this.updateEvents(this.state.locations, numberOfEvents)
+        )
+    }
+
     updateEvents = (location, eventCount) => {
         getEvents().then((events) => {
             const locationEvents =
@@ -36,18 +45,10 @@ class App extends Component {
                 this.setState({
                     events: locationEvents.slice(0, this.state.numberOfEvents),
                     currentLocation: location,
+                    numberOfEvents: eventCount,
                 })
             }
         })
-    }
-
-    updateNumberOfEvents = (numberOfEvents) => {
-        this.setState(
-            {
-                numberOfEvents,
-            },
-            this.updateEvents(this.state.locations, numberOfEvents)
-        )
     }
 
     render() {
